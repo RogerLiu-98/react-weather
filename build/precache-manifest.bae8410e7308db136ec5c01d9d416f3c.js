50df276a15a341924cc8b7585eaecccc2952a39e
Roger <u6924878@anu.edu.au>
2019-12-11 13:11:51 +1100

    update build

---
+++ build/precache-manifest.bae8410e7308db136ec5c01d9d416f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39486ffdb99e9d0c48e3971ed04376bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32cd906b4ef47b7fc39a",
+    "url": "./static/css/2.568a7919.chunk.css"
+  },
+  {
+    "revision": "53032e2a0e644636b4b6",
+    "url": "./static/css/main.67df3a20.chunk.css"
+  },
+  {
+    "revision": "32cd906b4ef47b7fc39a",
+    "url": "./static/js/2.f4488dba.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "./static/js/2.f4488dba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53032e2a0e644636b4b6",
+    "url": "./static/js/main.089647e2.chunk.js"
+  },
+  {
+    "revision": "14a92e1ae567590c349e",
+    "url": "./static/js/runtime-main.134f7680.js"
+  }
+]);
